518079e7fc7b1d410ffab7023fe72a25d4b75187
Neil B <neilbalch@gmail.com>
2017-06-29 20:40:26 -0700

    Add "Clean Adobe Bloat" Batch Script

---
+++ Adobe/AdobeBloat.cmd
@@ -0,0 +1,19 @@
+@ECHO OFF
+SET interactive=0
+
+ECHO %CMDCMDLINE% | FINDSTR /L %COMSPEC% >NUL 2>&1
+IF %ERRORLEVEL% == 0 SET interactive=1
+
+del /q C:\Users\%USERNAME%\AppData\Roaming\Adobe\Common\"Media Cache"\*
+@echo "Media Cache" Deleted...
+
+del /q C:\Users\%USERNAME%\AppData\Roaming\Adobe\Common\"Media Cache Files"\*
+@echo "Media Cache Files" Deleted...
+
+del /q C:\Users\%USERNAME%\AppData\Roaming\Adobe\Common\"Peak Files"\*
+@echo "Peak Files" Deleted...
+
+@pause
+
+IF "%interactive%"=="0" PAUSE
+EXIT /B 0
