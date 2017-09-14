f42d3f9b92f3097d86600b80d805014a7dcb18a9
Neil <neilbalch@gmail.com>
2017-09-13 18:15:16 -0700

    Mod IP Switcher to better suit usecase

--- RoboticsIPChanger.bat
+++ RoboticsIPChanger.bat
@@ -28,44 +28,75 @@ if '%errorlevel%' NEQ '0' (
 REM BEGIN!!!!
 @echo off 
 echo Choose: Home or Robotics 
-echo [R] Set Robotics Static IP 
-echo [H] Set Home DHCP IP 
+echo [SW] Set Robotics Static IP: WiFi 
+echo [SE] Set Robotics Static IP: Ethernet 
+echo [DW] Set Home DHCP IP: WiFi 
+echo [DE] Set Home DHCP IP: Ethernet
 echo. 
 
 :choice 
-SET /P C=[R,H]? 
-for %%? in (R) do if /I "%C%"=="%%?" goto Robotics
-for %%? in (H) do if /I "%C%"=="%%?" goto Home
+SET /P C=[SW,SE,DW,DE]? 
+for %%? in (SW) do if /I "%C%"=="%%?" goto SWiFi
+for %%? in (SE) do if /I "%C%"=="%%?" goto SEthernet
+for %%? in (DW) do if /I "%C%"=="%%?" goto DWiFi
+for %%? in (DE) do if /I "%C%"=="%%?" goto DEthernet
 
 goto choice 
 
-REM Set Robotics
-:Robotics
-set Eth_IP_Addr=10.9.71.55
-echo "Ethernet Static IP Address: "%&IPAddr%
+REM Set SEthernet
+:SEthernet
+set Eth_IP=10.9.71.55
+echo "Ethernet Static IP Address:" %Eth_IP%
 
-set WiFi_IP_Addr=10.9.71.56
-echo "WiFi Static IP Address: "%&IPAddr%
+REM Might be 10.0.0.1
+set Gate=10.9.71.1
+echo "Default Gateway:" %Gate%
 
-REM Might actually be 10.0.0.1
-set D_Gate=10.9.71.1
-echo "Default Gateway: "%D_Gate%
+set Mask=255.0.0.0
+echo "Subnet Mask:" %Mask%
 
-set Sub_Mask=255.0.0.0
-echo "Subnet Mask: "%Sub_Mask%
-
-echo "Setting: Robotics (Static IP)" 
-netsh interface ipv4 set address "WiFi" static %WiFi_IP_Addr% %Sub_Mask% %D_Gate% 1 
-netsh interface ipv4 set address "LAN" static %Eth_IP_Addr% %Sub_Mask% %D_Gate% 1 
+echo "Setting: Ethernet (Static IP)" 
+netsh interface ipv4 set address "LAN" static %Eth_IP% %Mask% %Gate% 
 netsh int ip show config 
 pause 
 goto end
 
-REM Set Home DHCP
-:Home
+REM Set SWiFi
+:SWiFi
+set WiFi_IP=10.9.71.56
+echo "WiFi Static IP Address:" %&WiFi_IP%
+
+REM Might be 10.0.0.1
+set Gate=10.9.71.1
+echo "Default Gateway:" %Gate%
+
+set Mask=255.0.0.0
+echo "Subnet Mask:" %Mask%
+
+echo "Setting: Wifi (Static IP)" 
+netsh interface ipv4 set address "WiFi" static %WiFi_IP% %Mask% %Gate% 
+netsh int ip show config 
+pause 
+goto end
+
+REM Set WiFi Home DHCP
+:DWiFi
 @ECHO OFF 
-ECHO "Setting: Home (DHCP)" 
+ECHO "Setting: WiFi (DHCP)" 
 netsh int ipv4 set address name = "WiFi" source = dhcp
+
+ipconfig /renew
+
+ECHO Here are the new settings for %computername%: 
+netsh int ip show config
+
+pause 
+goto end 
+
+REM Set Ethernet Home DHCP
+:DEthernet
+@ECHO OFF 
+ECHO "Setting: Ethernet (DHCP)" 
 netsh int ipv4 set address name = "LAN" source = dhcp
 
 ipconfig /renew
