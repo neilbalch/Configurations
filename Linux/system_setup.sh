cd5652eb9e9a12314813204d9e154a93359f257d
Neil <neilbalch@gmail.com>
2023-08-08 21:08:41 -0700

    Add Bazel build and other useful packages

--- Linux/system_setup.sh
+++ Linux/system_setup.sh
@@ -11,10 +11,22 @@ sudo apt update
 sudo apt full-upgrade -y
 sudo apt install -y \
   vim git cmake make ant openjdk-17-jre-headless \
+  python3 python3-pip \
   proot qemu-user-static qemu-utils \
-  stlink-tools \
-  neofetch gnome-system-monitor htop bmon iotop \
-  iperf3 qdirstat rsync screen x11-apps
+  stlink-tools openocd \
+  neofetch gnome-system-monitor htop bmon iotop qdirstat \
+  iperf3 qdirstat rsync screen tmux x11-apps xcowsay \
+  unattended-upgrades smartmontools
+sudo pip3 install black pyserial
+
+# Install Bazel
+# https://bazel.build/install/ubuntu
+sudo apt install apt-transport-https curl gnupg -y
+curl -fsSL https://bazel.build/bazel-release.pub.gpg | gpg --dearmor >bazel-archive-keyring.gpg
+sudo mv bazel-archive-keyring.gpg /usr/share/keyrings
+echo "deb [arch=amd64 signed-by=/usr/share/keyrings/bazel-archive-keyring.gpg] https://storage.googleapis.com/bazel-apt stable jdk1.8" | sudo tee /etc/apt/sources.list.d/bazel.list
+sudo apt update
+sudo apt install bazel
 
 # Install vscode
 wget "https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-x64" -O vscode.deb
@@ -68,3 +80,6 @@ fi
 # Enable mDNS (NOTE: a specific interface must still be attached manually!)
 sudo systemctl enable systemd-resolved.service
 sudo systemctl start systemd-resolved.service
+
+# Just for fun :)
+neofetch
